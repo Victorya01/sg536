589a56930c31c972f988877f8304bb68f0085cad
Victorya01 <76602663+Victorya01@users.noreply.github.com>
2021-03-09 15:06:14 +0300

    Create BinaryTree.cs

---
+++ BinaryTree.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAp0
+{
+    class BinaryTree
+    {
+        private TreeNode root;
+        public TreeNode Root
+        {
+            get { return root; }
+        }
+        public void Insert(int data)
+        {
+
+            if (root != null)
+            {
+                root.Insert(data);
+            }
+            else
+            {
+                root = new TreeNode(data);
+            }
+        }
+        public void PreorderTraversal()
+        {
+            if (root != null)
+                root.PreOrderTraversal();
+        }
+        public void PostorderTraversal()
+        {
+            if (root != null)
+                root.PostorderTraversal();
+        }
+        public int Height()
+        {
+            if (root == null)
+            { return 0; }
+
+            return root.Height();
+        }
+        public bool IsBalanced()
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            return root.IsBalanced();
+        }
+
+    }
+}
